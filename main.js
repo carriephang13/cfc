80d5a95bca5487423ca249731a30191fbe25e5df
carriephang13 <37949250+carriephang13@users.noreply.github.com>
2018-06-04 03:06:02 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,39 @@ function playMusic(){
   }
 }
 $("#playBom").click(playMusic)
+
+function dimple(){
+  if (playing == 1){
+    $("#dimple").trigger("pause");
+    $(".play").addClass("hidden")
+    $(".pause").removeClass("hidden")
+  }
+  if (playing == 0){
+    $("#dimple").trigger("play");
+    $(".pause").addClass("hidden")
+    $(".play").removeClass("hidden")
+  }
+  playing = playing + 1;
+  if (playing > 1){
+    playing = 0;
+  }
+}
+$("#playDimple").click(dimple)
+
+function dna(){
+  if (playing == 1){
+    $("#dna").trigger("pause");
+    $(".play").addClass("hidden")
+    $(".pause").removeClass("hidden")
+  }
+  if (playing == 0){
+    $("#dna").trigger("play");
+    $(".pause").addClass("hidden")
+    $(".play").removeClass("hidden")
+  }
+  playing = playing + 1;
+  if (playing > 1){
+    playing = 0;
+  }
+}
+$("#playDna").click(dna)
