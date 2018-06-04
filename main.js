a2ac4e4c2f3a7e43b1da03d255b200eb08431186
carriephang13 <37949250+carriephang13@users.noreply.github.com>
2018-06-04 03:19:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -52,3 +52,111 @@ function playMusic2(){
   }
 }
 $("#playDna").click(playMusic2)
+
+function playMusic3(){
+  if (playing == 1){
+    $("#gogo").trigger("pause");
+    $(".play").addClass("hidden")
+    $(".pause").removeClass("hidden")
+  }
+  if (playing == 0){
+    $("#gogo").trigger("play");
+    $(".pause").addClass("hidden")
+    $(".play").removeClass("hidden")
+  }
+  playing = playing + 1;
+  if (playing > 1){
+    playing = 0;
+  }
+}
+$("#playGogo").click(playMusic3)
+
+function playMusic4(){
+  if (playing == 1){
+    $("#her").trigger("pause");
+    $(".play").addClass("hidden")
+    $(".pause").removeClass("hidden")
+  }
+  if (playing == 0){
+    $("#her").trigger("play");
+    $(".pause").addClass("hidden")
+    $(".play").removeClass("hidden")
+  }
+  playing = playing + 1;
+  if (playing > 1){
+    playing = 0;
+  }
+}
+$("#playHer").click(playMusic4)
+
+function playMusic5(){
+  if (playing == 1){
+    $("#md").trigger("pause");
+    $(".play").addClass("hidden")
+    $(".pause").removeClass("hidden")
+  }
+  if (playing == 0){
+    $("#md").trigger("play");
+    $(".pause").addClass("hidden")
+    $(".play").removeClass("hidden")
+  }
+  playing = playing + 1;
+  if (playing > 1){
+    playing = 0;
+  }
+}
+$("#playMd").click(playMusic5)
+
+function playMusic6(){
+  if (playing == 1){
+    $("#piedPiper").trigger("pause");
+    $(".play").addClass("hidden")
+    $(".pause").removeClass("hidden")
+  }
+  if (playing == 0){
+    $("#piedPiper").trigger("play");
+    $(".pause").addClass("hidden")
+    $(".play").removeClass("hidden")
+  }
+  playing = playing + 1;
+  if (playing > 1){
+    playing = 0;
+  }
+}
+$("#playPiedPiper").click(playMusic6)
+
+function playMusic7(){
+  if (playing == 1){
+    $("#serendipity").trigger("pause");
+    $(".play").addClass("hidden")
+    $(".pause").removeClass("hidden")
+  }
+  if (playing == 0){
+    $("#serendipity").trigger("play");
+    $(".pause").addClass("hidden")
+    $(".play").removeClass("hidden")
+  }
+  playing = playing + 1;
+  if (playing > 1){
+    playing = 0;
+  }
+}
+$("#playSerendipity").click(playMusic7)
+
+function playMusic8(){
+  if (playing == 1){
+    $("#skit").trigger("pause");
+    $(".play").addClass("hidden")
+    $(".pause").removeClass("hidden")
+  }
+  if (playing == 0){
+    $("#skit").trigger("play");
+    $(".pause").addClass("hidden")
+    $(".play").removeClass("hidden")
+  }
+  playing = playing + 1;
+  if (playing > 1){
+    playing = 0;
+  }
+}
+$("#playSkit").click(playMusic8)
